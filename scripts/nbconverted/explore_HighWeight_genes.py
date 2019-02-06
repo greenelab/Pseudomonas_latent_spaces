619a3a43662be4281a96c7af6efe6d75eac138e8
ajlee21 <alexjlee.21@gmail.com>
2019-02-06 12:37:24 -0500

    Add new scripts to explore simulated data

---
+++ scripts/nbconverted/explore_HighWeight_genes.py
@@ -0,0 +1,242 @@
+
+# coding: utf-8
+
+# # Explore latent space features
+# We want to know if our VAE model is capturing our signal
+# 
+# Identify high weight (HW) genes for each latent space feature (node)
+# Determine if genes in group A and B are found within these highly weighted genes
+
+# In[2]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr, spearmanr
+
+
+randomState = 123
+from numpy.random import seed
+seed(randomState)
+
+
+# In[3]:
+
+
+# Load arguments
+analysis_name = "sim_AB_2775_300_v2"
+data_file = os.path.join(os.path.dirname(os.getcwd()), "data", analysis_name, "VAE_weight_matrix.txt")
+A_file = os.path.join(os.path.dirname(os.getcwd()), "data", analysis_name, "geneSetA.txt")
+B_file = os.path.join(os.path.dirname(os.getcwd()), "data", analysis_name, "geneSetB.txt")
+
+#HW_file = os.path.join(os.path.dirname(os.getcwd()), "output", analysis_name, "HW_genes.txt")
+
+
+# In[4]:
+
+
+# Read in data
+geneSetA = pd.read_table(A_file, header=0, index_col=0)
+geneSetB = pd.read_table(B_file, header=0, index_col=0)
+
+weight = pd.read_table(data_file, header=0, index_col=0).T
+weight.head(5)
+
+
+# In[5]:
+
+
+# Plot the distribution of gene weights per latent feature
+#num_features = weight.shape[1]
+
+#f, axes = plt.subplots(num_features, 1, sharex=True)
+#for i, ax in zip(weight.columns, axes.flat):
+#    sns.distplot(weight[i] , color="skyblue", rug=False, kde=False, ax=ax)
+    
+#f.set_size_inches(15, 15)
+
+
+# In[6]:
+
+
+# Calculate mean per node ("signature" or "feature")
+means = weight.mean(axis=0)
+
+# Calculate 2 standard deviations per node ("signature" or "feature")
+stds = weight.std(axis=0)
+two_stds = 2*stds
+
+
+# In[7]:
+
+
+# Get high positive and negative weight genes per node ("signature" or "feature") -- HW_df
+# Record metadata per latent feature in a table -- HW_metadata_df
+# Record distance between gene set weight and 
+HW_df = pd.DataFrame()
+HW_metadata_df = pd.DataFrame()
+
+num_nodes = len(means)
+
+for i in range(num_nodes):
+    node_mean = means.iloc[i]
+    node_std = stds.iloc[i] 
+    
+    pos_threshold = node_mean + two_stds.iloc[i]
+    neg_threshold = node_mean - two_stds.iloc[i]
+    
+    hw_pos_genes = weight[weight[i+1] > pos_threshold].index
+    hw_neg_genes = weight[weight[i+1] < neg_threshold].index
+    
+    node = str(i+1)
+    node_name = 'Sig'+node+'pos'
+    
+    # Add high weight positive genes
+    add_pos = pd.DataFrame({node_name: list(hw_pos_genes)})
+    HW_df = pd.concat([HW_df, add_pos], axis=1)
+    
+    # Add metadata for positive node
+    add_metadata_pos = pd.DataFrame({node_name: [node_mean, node_std, pos_threshold]})
+    HW_metadata_df = pd.concat([HW_metadata_df, add_metadata_pos], axis=1)
+    
+    # Add high weight negative genes
+    node_name = 'Sig'+node+'neg'
+    
+    add_neg = pd.DataFrame({node_name: list(hw_neg_genes)})
+    HW_df = pd.concat([HW_df, add_neg], axis=1)
+    
+    # Add metadata for negative node
+    add_metadata_neg = pd.DataFrame({node_name: [node_mean, node_std, neg_threshold]})
+    HW_metadata_df = pd.concat([HW_metadata_df, add_metadata_neg], axis=1)
+
+HW_df = HW_df.T
+HW_metadata_df = HW_metadata_df.T
+
+
+# In[8]:
+
+
+HW_df.head()
+
+
+# In[9]:
+
+
+# Dataframe with the mean, std, threshold used per feature to determine high weight gene sets
+HW_metadata_df.columns = ["mean", "std", "threshold"]
+HW_metadata_df.head()
+
+
+# In[10]:
+
+
+# Create a table (feature x gene set A)
+# Each cell will contain the distance between the weight and the mean
+
+Weights_pos_neg_df = pd.DataFrame()
+
+for i in range(num_nodes):
+    node_mean = means.iloc[i]
+    
+    node = str(i+1)
+    node_name = 'Sig'+node+'pos'
+    
+    gene_weight = weight[i+1]
+    
+    # Add high weight positive genes
+    add_pos = pd.DataFrame({node_name: gene_weight})
+    Weights_pos_neg_df = pd.concat([Weights_pos_neg_df, add_pos], axis=1)
+    
+    # Add high weight negative genes
+    node_name = 'Sig'+node+'neg'
+    
+    add_neg = pd.DataFrame({node_name: gene_weight})
+    Weights_pos_neg_df = pd.concat([Weights_pos_neg_df, add_neg], axis=1)
+    
+    
+Weights_pos_neg_df = Weights_pos_neg_df.T
+Weights_pos_neg_df.head()
+
+
+# In[11]:
+
+
+# Compare the weights for geneset A with threshold
+geneSetA_ls = [l[0] for l in geneSetA.values.tolist()]
+Weight_A = Weights_pos_neg_df[geneSetA_ls]
+
+Weight_A.head()
+
+
+# In[12]:
+
+
+sns.distplot(Weight_A.iloc[0])
+
+
+# In[13]:
+
+
+# Compare the weights for geneset B with threshold
+geneSetB_ls = [l[0] for l in geneSetB.values.tolist()]
+Weight_B = Weights_pos_neg_df[geneSetB_ls]
+
+Weight_B.head()
+
+
+# In[14]:
+
+
+sns.distplot(Weight_B.iloc[0])
+
+
+# In[15]:
+
+
+# What is the overlap between the high weight genes and gene sets A and B?
+num_A = geneSetA.shape[0]
+num_B = geneSetB.shape[0]
+
+num_features_pos_neg = HW_df.shape[0]
+
+percent_overlap = pd.DataFrame({'feature': [], 
+                                'percent in A': [],
+                                'percent in B': []
+                               })
+
+for i in range(num_features_pos_neg):
+#for i in range(2):
+    row = HW_df.iloc[i]
+    percent_in_A = (row.isin(geneSetA_ls).sum()/num_A)*100
+    percent_in_B = (row.isin(geneSetB_ls).sum()/num_B)*100
+    
+    add = pd.DataFrame({'feature': [HW_df.index[i]],
+                        'percent in A': [percent_in_A],
+                        'percent in B': [percent_in_B]
+                       })
+    percent_overlap = percent_overlap.append(add)
+
+percent_overlap
+
+
+# In[16]:
+
+
+# Are there any features that are nonzero?
+overlap_in_A = percent_overlap.iloc[percent_overlap["percent in A"].nonzero()[0]]
+overlap_in_B = percent_overlap.iloc[percent_overlap["percent in B"].nonzero()[0]]
+
+print(overlap_in_A.shape)
+print(overlap_in_B.shape)
+overlap_in_A
+
+
+# In[17]:
+
+
+overlap_in_B
+
---
+++ scripts/nbconverted/explore_offset.py
@@ -0,0 +1,259 @@
+
+# coding: utf-8
+
+# # Explore offset vector
+# 
+# We want to know what the offset vector is capturing.  Theoretically it should be capturing the "essence of gene A" since it is defined by taking the samples with the highest expression of gene A and the lowest expression of gene A.
+# 
+# We want to test if this offset vector is capturing genes in group A and B
+
+# In[ ]:
+
+
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib_venn import venn2
+
+randomState = 123
+from numpy.random import seed
+seed(randomState)
+
+
+# In[2]:
+
+
+# Load data
+base_dir = os.path.join(os.path.dirname(os.getcwd()), "data")
+analysis_name = "sim_AB_2775_300_v2"
+offset_gene_file = os.path.join(os.path.dirname(os.getcwd()), "data", analysis_name, "offset_gene_space.txt")
+offset_vae_file = os.path.join(os.path.dirname(os.getcwd()), "encoded", analysis_name, "offset_latent_space_vae.txt")
+A_file = os.path.join(base_dir, analysis_name, "geneSetA.txt")
+B_file = os.path.join(base_dir, analysis_name, "geneSetB.txt")
+weight_file = os.path.join(os.path.dirname(os.getcwd()), "data", analysis_name, "VAE_weight_matrix.txt")
+
+
+# In[3]:
+
+
+# Read gene space offset
+offset_gene_space = pd.read_table(offset_gene_file, header=0, index_col=0)
+offset_gene_space
+
+
+# In[4]:
+
+
+# Read VAE space offset
+offset_vae_space = pd.read_table(offset_vae_file, header=0, index_col=0)
+offset_vae_space
+
+
+# In[5]:
+
+
+# Read genes in set A
+geneSetA = pd.read_table(A_file, header=0, index_col=0)
+geneSetA_ls = [l[0] for l in geneSetA.values.tolist()]
+geneSetA_set = set(geneSetA_ls)
+
+
+# In[6]:
+
+
+# Read genes in set B
+geneSetB = pd.read_table(B_file, header=0, index_col=0)
+geneSetB_ls = [l[0] for l in geneSetB.values.tolist()]
+geneSetB_set = set(geneSetB_ls)
+
+
+# In[7]:
+
+
+# Read weight matrix
+weight = pd.read_table(weight_file, header=0, index_col=0).T
+weight.head(5)
+
+
+# ## Explore gene space offset
+# 
+# 1.  What genes are most highly weighted?
+# 2.  What percentage of these genes are in gene set A and B?
+
+# In[8]:
+
+
+# Distribution of weights in offset vector
+sns.distplot(offset_gene_space)
+
+
+# In[9]:
+
+
+# Get gene ids with the highest weight from the offset vector
+percentile = 95
+threshold = np.percentile(offset_gene_space, percentile)
+print("Threshold cutoff is {}".format(threshold))
+highest_genes = offset_gene_space.T[offset_gene_space.T[0] > threshold].index
+
+
+# In[10]:
+
+
+# Compare the overlap of genes in set A and highest weighted genes in offset
+venn2([set(highest_genes), geneSetA_set], set_labels = ('High weight offset genes', 'Group A genes'))
+plt.show()
+
+
+# In[11]:
+
+
+# Compare the overlap of genes in set B and highest weighted genes in offset
+venn2([set(highest_genes), geneSetB_set], set_labels = ('High weight offset genes', 'Group B genes'))
+plt.show()
+
+
+# ## Explore latent space (VAE) offset
+# 1.  Which feature has the highest value?
+# 2.  Are genes in set A and B highly weighted 
+
+# In[12]:
+
+
+# Distribution of weights in offset vector
+sns.distplot(offset_vae_space)
+
+
+# In[13]:
+
+
+# Get latent feature with the max and min value
+max_value = max(offset_vae_space.T[0])
+min_value = min(offset_vae_space.T[0])
+
+max_feature = offset_vae_space.T[offset_vae_space.T[0] == max_value].index[0]
+min_feature = offset_vae_space.T[offset_vae_space.T[0] == min_value].index[0]
+print("Max feature is {} and min feature is {}".format(max_feature, min_feature))
+
+
+# In[14]:
+
+
+# Get gene weights for max latent feature
+genes_max_feature = weight[int(max_feature)]
+sns.distplot(genes_max_feature)
+
+
+# In[15]:
+
+
+# Get gene ids with the highest positive weight from the max feature selected
+percentile = 95
+threshold = np.percentile(genes_max_feature, percentile)
+print("Threshold cutoff is {}".format(threshold))
+highest_genes = genes_max_feature[genes_max_feature > threshold].index
+
+
+# In[16]:
+
+
+# Get gene ids with the highest negative weight from the max feature selected
+percentile = 5
+threshold = np.percentile(genes_max_feature, percentile)
+print("Threshold cutoff is {}".format(threshold))
+lowest_genes = genes_max_feature[genes_max_feature < threshold].index
+
+
+# In[17]:
+
+
+# Compare the overlap of genes in set A and highest positive weighted genes in the max feature
+venn2([set(highest_genes), geneSetA_set], set_labels = ('High positive weight genes in feature {}'.format(max_feature), 'Group A genes'))
+plt.show()
+
+
+# In[18]:
+
+
+# Compare the overlap of genes in set B and highest positive weighted genes in the max feature
+venn2([set(highest_genes), geneSetB_set], set_labels = ('High positive weight genes in feature {}'.format(max_feature), 'Group B genes'))
+plt.show()
+
+
+# In[28]:
+
+
+# Compare the overlap of genes in set A and highest negative weighted genes in the max feature
+venn2([set(lowest_genes), geneSetA_set], set_labels = ('High negative weight genes in feature {}'.format(max_feature), 'Group A genes'))
+plt.show()
+
+
+# In[29]:
+
+
+# Compare the overlap of genes in set B and highest negative weighted genes in the max feature
+venn2([set(lowest_genes), geneSetB_set], set_labels = ('High negative weight genes in feature {}'.format(max_feature), 'Group B genes'))
+plt.show()
+
+
+# In[21]:
+
+
+# Get gene weights for min latent feature
+genes_min_feature = weight[int(min_feature)]
+sns.distplot(genes_min_feature)
+
+
+# In[22]:
+
+
+# Get gene ids with the highest positive weight from the min feature selected
+percentile = 95
+threshold = np.percentile(genes_min_feature, percentile)
+print("Threshold cutoff is {}".format(threshold))
+highest_genes = genes_min_feature[genes_min_feature > threshold].index
+
+
+# In[23]:
+
+
+# Get gene ids with the highest negative weight from the min feature selected
+percentile = 5
+threshold = np.percentile(genes_min_feature, percentile)
+print("Threshold cutoff is {}".format(threshold))
+lowest_genes = genes_min_feature[genes_min_feature < threshold].index
+
+
+# In[33]:
+
+
+# Compare the overlap of genes in set A and highest positive weighted genes in the min feature
+venn2([set(highest_genes), geneSetA_set], set_labels = ('High negative weight genes in feature {}'.format(min_feature), 'Group A genes'))
+plt.show()
+
+
+# In[32]:
+
+
+# Compare the overlap of genes in set B and highest positive weighted genes in the min feature
+venn2([set(highest_genes), geneSetB_set], set_labels = ('High negative weight genes in feature {}'.format(min_feature), 'Group B genes'))
+plt.show()
+
+
+# In[30]:
+
+
+# Compare the overlap of genes in set A and highest negative weighted genes in the min feature
+venn2([set(lowest_genes), geneSetA_set], set_labels = ('High negative weight genes in feature {}'.format(min_feature), 'Group A genes'))
+plt.show()
+
+
+# In[31]:
+
+
+# Compare the overlap of genes in set B and highest negative weighted genes in the min feature
+venn2([set(lowest_genes), geneSetB_set], set_labels = ('High negative weight genes in feature {}'.format(min_feature), 'Group B genes'))
+plt.show()
+
